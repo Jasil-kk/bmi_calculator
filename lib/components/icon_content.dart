47d3336132f5cf453f19d7841f39e8fce15ad657
Jasil-kk <jasikk2522@gmail.com>
2023-06-13 12:15:24 +0530

    calculator ui completed

--- lib/components/icon_content.dart
+++ lib/components/icon_content.dart
@@ -1,6 +1,5 @@
-
-
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../constants.dart';
 
 class IconContent extends StatelessWidget {
   IconContent({this.icon, this.label});
@@ -13,7 +12,7 @@ class IconContent extends StatelessWidget {
       children: [
         Icon(icon,size: 70.0,),
         SizedBox(height: 10.0,),
-        Text(label! ,style: TextStyle(fontSize: 18.0,color: Color(0xFFF5F5F6)),)
+        Text(label! ,style:kLabelTextStyle,)
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'input_page.dart';
+import 'screens/input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -9,7 +9,7 @@ class BMICalculator extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(bodyText1: TextStyle(fontFamily: 'Outfit'),bodyText2: TextStyle(fontFamily: 'Outfit')),
+        textTheme: TextTheme(bodyLarge: TextStyle(fontFamily: 'Outfit'),bodyMedium: TextStyle(fontFamily: 'Outfit')),
         appBarTheme: AppBarTheme(
           titleTextStyle: TextStyle(fontFamily: 'Outfit',fontWeight: FontWeight.w600,letterSpacing: 1.0),
           backgroundColor: Color(0xFF0A0D22),
